f56a656063098626c15df508cc794ce12a5c8b55
Jannis Hübl <jannis.huebl@metoda.com>
2016-06-28 11:25:06 +0200

    remove log

--- lib/crono/orm/active_record/crono_job.rb
+++ lib/crono/orm/active_record/crono_job.rb
@@ -58,24 +58,13 @@ module Crono
     private
 
     def perform_job
-      logger = Logger.new(File.join(Rails.root, 'log', 'crono_output.log'))
-      logger.info "perform job"
-      logger.info performer
-      logger.info args
       performer.constantize.new.perform *args
-      logger.info "performed_job"
     rescue StandardError => e
-      logger.info "cachted error"
-      logger.info e
-      logger.info e.backtrace.join("\n")
       handle_job_fail(e)
     else
-      logger.info "perform succesed"
       handle_job_success
     ensure
-      logger.info "perform ensure"
       save
-      logger.info "perform ensure2"
     end
 
     def clear_job_log
@@ -102,10 +91,6 @@ module Crono
     end
 
     def log_message(message, severity = Logger::INFO)
-      stdout_logger = Logger.new(STDOUT)
-      stdout_logger.level = Logger::WARN
-      stdout_logger.log severity, message
-      logger.log severity, message
       self.job_logger ||= Rails.logger
       self.job_logger.log severity, message
     end
